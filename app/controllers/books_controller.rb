6e6c7ca6d25ed25d8e9f3d62cc9da1d63c6d0d9c
HidekiShiba <hshiba033@gmail.com>
2020-11-16 09:07:30 +0000

    [Add] 投稿/編集エラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  
   def top
   end
 
@@ -12,12 +13,13 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    if book.save
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
-      render :new
+      render :index
     end
   end
 
@@ -26,15 +28,18 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
